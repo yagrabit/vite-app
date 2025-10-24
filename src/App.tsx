b6d8e729b06dcfa269ebf96353d660c62b6d5174
yagrabit <156518458+yagrabit@users.noreply.github.com>
2025-10-24 09:29:51 +0900

    feat: add environment variable for app title and update App component to use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const appTitle = import.meta.env.VITE_APP_TITLE || 'Vite App';
+console.dir(import.meta.env);
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -16,18 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{appTitle}</h1>
     </>
   )
 }
---
+++ src/vite-env.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv {
+    readonly TITLE: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
+
