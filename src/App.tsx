9ecfe0797c98fd6ed1380d4a68d4bfd95dd3ee3d
yagrabit <156518458+yagrabit@users.noreply.github.com>
2025-10-25 09:17:02 +0900

    feat: add Button component and integrate it into App with state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 
 const appTitle = import.meta.env.VITE_APP_TITLE || 'Vite App';
 console.dir(import.meta.env);
 
 function App() {
+
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,6 +22,9 @@ function App() {
         </a>
       </div>
       <h1>{appTitle}</h1>
+      <div className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+      </div>
     </>
   )
 }
